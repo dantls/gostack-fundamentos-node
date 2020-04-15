8576acdaca7a4c81b09f1ead02d59f6dd186a753
dantls <dan_tls@hotmail.com>
2020-04-15 04:13:18 -0400

    challenge finished

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -5,6 +5,13 @@ interface Balance {
   outcome: number;
   total: number;
 }
+interface CreateTransactionDTO {
+  title: string;
+
+  value: number;
+
+  type: 'income' | 'outcome';
+}
 
 class TransactionsRepository {
   private transactions: Transaction[];
@@ -13,16 +20,39 @@ class TransactionsRepository {
     this.transactions = [];
   }
 
-  public all(): Transaction[] {
-    // TODO
-  }
-
   public getBalance(): Balance {
-    // TODO
+    const { income, outcome } = this.transactions.reduce(
+      (acc, transaction) => {
+        acc[transaction.type] += transaction.value;
+        return acc;
+      },
+      { income: 0, outcome: 0 },
+    );
+
+    const balance = {
+      income,
+      outcome,
+      total: income - outcome,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public all(): Transaction[] {
+    return this.transactions;
+  }
+
+  public validBalance({ title, value, type }: CreateTransactionDTO): boolean {
+    const balance = this.getBalance();
+    return type === 'outcome' && value > balance.total;
+  }
+
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
